8078f16272abeda416e7696311279c7a5bb3f12f
Jonathan D.A. Jewell <jonathan.jewell@open.ac.uk>
2026-02-01 01:50:07 +0000

    chore: sync

--- .machine_readable/NEUROSYM.scm
+++ .machine_readable/NEUROSYM.scm
@@ -1,4 +1,4 @@
-;; SPDX-License-Identifier: AGPL-3.0-or-later
+;; SPDX-License-Identifier: MPL-2.0-or-later
 ;; NEUROSYM.scm - Neurosymbolic integration config for zotero-nesy
 
 (define neurosym-config
